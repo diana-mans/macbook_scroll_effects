397d94d45fd4c3cb1e2cc64c197a84abc16f5e97
Диана Мансурова <diana.mans2000@gmail.com>
2026-05-12 13:43:14 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import NavBar from "./components/NavBar.jsx";
+
+const App = () => {
+    return (
+        <main className="App">
+            <NavBar />
+        </main>
+    )
+}
+
+export default App;
